f466ecb1aad1b3f82c75ac71bdb9d68925b8f9b1
fore447 <mmori.0426@gmail.com>
2023-08-19 22:15:38 +0900

    👍[TOP]TableViewをAutoLayoutで実装
    【対応内容】
    ・TableViewをAutoLayoutで実装
    
    【影響範囲】
    ・TOP画面
    
    【動作確認】
    ・TableViewが表示される
    
    # ==== Emojis ====
    # 🐝 バグ修正
    # 👍 機能改善
    # ✨ 部分的な機能追加
    # 🎨 デザイン変更のみ
    # 💢 コンフリクト
    # 🚧 WIP
    # 📝 文言修正
    # ♻️ リファクタリング
    # 🔥 不要な機能・使われなくなった機能の削除
    # 💚 テストやCIの修正・改善
    # 👕 Lintエラーの修正やコードスタイルの修正
    # 🚀 パフォーマンス改善
    # 📂 ファイル追加
    # 🆙 依存パッケージなどのアップデート
    # 👮 セキュリティ関連の改善
    # ⚙  config変更
    # 📚 ドキュメント

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -7,21 +7,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // Tableで使用する配列を設定する
+    private let myItems:[String] = ["Todo1", "Todo2", "Todo3"]
+    private var myTableView: UITableView!
     
-    private let nameLabel = UILabel()
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        nameLabel.text = "Todo"
-        nameLabel.sizeToFit()
-        nameLabel.frame.origin.y = 50
-        nameLabel.frame.origin.x = 50
         
-        view.addSubview(nameLabel)
+        super.viewDidLoad()
+        myTableView = UITableView()
+        
+        // Cell名の登録をおこなう.
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする.
+        myTableView.dataSource = self
+        
+        // Delegateを設定する.
+        myTableView.delegate = self
+        
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Viewに追加する.
+        self.view.addSubview(myTableView)
+        
+        setupLayout()
     }
-
-
+    
+    private func setupLayout() {
+        myTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        myTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        myTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        myTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    // 行が選択されていることをdelegateに伝える
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row:\(indexPath.section)")
+        print("Section: \(indexPath.row)")
+        print("Value: \(myItems[indexPath.row])")
+    }
+    
+    // 指定された場所の行に使用する高さをデリゲートに尋ねる
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    // セルの行数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    // セルの表示設定を行う
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        // Configure the cell’s contents.
+        cell.textLabel!.text = myItems[indexPath.row]
+            
+        return cell
+    }
 }
-
