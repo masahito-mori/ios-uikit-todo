2986a68760f9388bc0212730c6fcf5a69ee111cb
fore447 <mmori.0426@gmail.com>
2023-09-09 18:25:50 +0900

    ♻️[TOP]クラスをfinalで宣言するように修正
    【対応内容】
    ・クラスをfinalで宣言するように修正
    
    【影響範囲】
    ・TOP
    
    【動作確認】
    ・正常に動作することを確認
    
    # ==== Emojis ====
    # 🐝 バグ修正
    # 👍 機能改善
    # ✨ 部分的な機能追加
    # 🎨 デザイン変更のみ
    # 💢 コンフリクト
    # 🚧 WIP
    # 📝 文言修正
    # ♻️ リファクタリング
    # 🔥 不要な機能・使われなくなった機能の削除
    # 💚 テストやCIの修正・改善
    # 👕 Lintエラーの修正やコードスタイルの修正
    # 🚀 パフォーマンス改善
    # 📂 ファイル追加
    # 🆙 依存パッケージなどのアップデート
    # 👮 セキュリティ関連の改善
    # ⚙  config変更
    # 📚 ドキュメント

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     private var myTableView = UITableView()
     private var todoListAdapter = TodoListAdapter()
     
