fdd53a70c53522b3bd8e9cd964e043d334e5e3dc
fore447 <mmori.0426@gmail.com>
2023-09-09 18:07:42 +0900

    ♻️[TOP]セルの生成、タップ時の処理をAdapterに切り出し
    【対応内容】
    ・セルの生成、タップ時の処理をAdapterに切り出し
    
    【影響範囲】
    ・TOP
    
    【動作確認】
    ・正常に描画、動作することを確認
    
    # ==== Emojis ====
    # 🐝 バグ修正
    # 👍 機能改善
    # ✨ 部分的な機能追加
    # 🎨 デザイン変更のみ
    # 💢 コンフリクト
    # 🚧 WIP
    # 📝 文言修正
    # ♻️ リファクタリング
    # 🔥 不要な機能・使われなくなった機能の削除
    # 💚 テストやCIの修正・改善
    # 👕 Lintエラーの修正やコードスタイルの修正
    # 🚀 パフォーマンス改善
    # 📂 ファイル追加
    # 🆙 依存パッケージなどのアップデート
    # 👮 セキュリティ関連の改善
    # ⚙  config変更
    # 📚 ドキュメント

---
+++ Todo/TodoListAdapter.swift
@@ -0,0 +1,43 @@
+//
+//  TodoListAdapter.swift
+//  Todo
+//
+//  Created by 森匡人 on 2023/09/09.
+//
+
+import UIKit
+
+final class TodoListAdapter: NSObject {
+    // Tableで使用する配列を設定する
+    private let myItems:[String] = ["Todo1", "Todo2", "Todo3"]
+}
+
+extension TodoListAdapter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+
+        // Configure the cell’s contents.
+        cell.textLabel?.text = myItems[indexPath.row]
+
+        return cell
+    }
+}
+
+extension TodoListAdapter: UITableViewDelegate {
+    // 指定された場所の行に使用する高さをデリゲートに尋ねる
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    // 行が選択されていることをdelegateに伝える
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row:\(indexPath.section)")
+        print("Section: \(indexPath.row)")
+        print("Value: \(myItems[indexPath.row])")
+    }
+}
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -7,31 +7,26 @@
 
 import UIKit
 
-
-
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    // Tableで使用する配列を設定する
-    private let myItems:[String] = ["Todo1", "Todo2", "Todo3"]
-    private var myTableView: UITableView!
+class ViewController: UIViewController {
+    private var myTableView = UITableView()
+    private var todoListAdapter = TodoListAdapter()
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        myTableView = UITableView()
-        
         // Cell名の登録をおこなう.
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
         
         // DataSourceの設定をする.
-        myTableView.dataSource = self
+        myTableView.dataSource = todoListAdapter
         
         // Delegateを設定する.
-        myTableView.delegate = self
+        myTableView.delegate = todoListAdapter
         
         myTableView.translatesAutoresizingMaskIntoConstraints = false
         
         // Viewに追加する.
-        self.view.addSubview(myTableView)
+        view.addSubview(myTableView)
         
         setupLayout()
     }
@@ -41,33 +36,5 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         myTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         myTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         myTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }
-    
-    // 行が選択されていることをdelegateに伝える
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Row:\(indexPath.section)")
-        print("Section: \(indexPath.row)")
-        print("Value: \(myItems[indexPath.row])")
-    }
-    
-    // 指定された場所の行に使用する高さをデリゲートに尋ねる
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    // セルの行数を返す
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myItems.count
-    }
-    
-    // セルの表示設定を行う
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Fetch a cell of the appropriate type.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-        
-        // Configure the cell’s contents.
-        cell.textLabel!.text = myItems[indexPath.row]
-            
-        return cell
     }
 }
